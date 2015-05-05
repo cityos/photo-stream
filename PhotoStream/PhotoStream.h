bd7e1b136addf5b96ca7084a39b946d4cc1f8c23
SaidSikira <saidsikira@gmail.com>
2015-05-05 13:44:52 +0200

    Initial project setup

---
+++ PhotoStream/PhotoStream.h
@@ -0,0 +1,19 @@
+//
+//  PhotoStream.h
+//  PhotoStream
+//
+//  Created by Said Sikira on 5/5/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoStream.
+FOUNDATION_EXPORT double PhotoStreamVersionNumber;
+
+//! Project version string for PhotoStream.
+FOUNDATION_EXPORT const unsigned char PhotoStreamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoStream/PublicHeader.h>
+
+
